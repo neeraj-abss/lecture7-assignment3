9c1710fbd00765fb9511ca4c1fe21cbca72670b8
MishraN <mishran@advisory.com>
2016-05-09 11:32:29 +0530

    null pointer exception

---
+++ assignment3.java
@@ -0,0 +1,12 @@
+class assignment3
+{
+public static void main(String args[])
+{
+	try{
+		String str=null;
+		System.out.println (str.length());
+	}catch(NullPointerException e){
+		System.out.println("NullPointerException..");
+	}
+}
+}
